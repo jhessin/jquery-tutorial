5541fba2d0f8b94c241fff7527accc373276289c
Jim Hessin <jhessin@gmail.com>
2021-05-14 23:15:51 -0600

    Basics of jquery

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
-import _ from 'lodash'
+import _ from 'lodash';
+import $ from 'jquery';
 
-function component() {
+function component(message: string) {
   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['<p>', message, '</p>'], ' ');
 
   return element;
 }
 
+function print(message: string) {
+  document.body.appendChild(component(message));
+}
 
-document.body.appendChild(component());
+//print('hello');
+
+$(document).ready(() => {
+  //$('#content').html('<strong>Hello World</strong>');
+  $('#content >>> .non-solid').addClass('highlight');
+});
+
