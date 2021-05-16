604646f621528572a11c882f2797029bedbf2555
Jim Hessin <jhessin@gmail.com>
2021-05-15 20:45:12 -0600

    Completed lesson 13

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,14 @@
 import $ from 'jquery';
 
 $(document).ready(() => {
-  //$('.box').on('click', function () {
-  //$(this).toggleClass('highlight');
-  //});
-  $('.box').on('click', '.box-button', event => {
-    $(event.toElement).parent().toggleClass('highlight');
+  $('#select-menu').on('change', function () {
+    const distance = $('#select-menu option:selected').val();
+    const name = $('#select-menu option:selected').text();
+    const price = $('#select-menu option:selected').data('price');
+    if (distance)
+      $('#feedback-message').text(
+        `You are signing up for a ${name}, which costs $${price}, for a distance of ${distance}km`,
+      );
+    else $('#feedback-message').text('');
   });
 });
