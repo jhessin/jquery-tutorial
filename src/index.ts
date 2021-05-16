df75be36bc760acfbea52bf83e1178c1e35249eb
Jim Hessin <jhessin@gmail.com>
2021-05-15 20:57:21 -0600

    Finished lesson 15

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,12 @@ import $ from 'jquery';
 $(document).ready(() => {
   $('#input-name').on('keyup', function () {
     const name = $(this).val();
-    if (name)
-    $('#feedback-message').text(`Pleased to meet you, ${name}`);
-    else
-    $('#feedback-message').text('');
+    if (name) $('#feedback-message').text(`Pleased to meet you, ${name}`);
+    else $('#feedback-message').text('');
+  });
+
+  $('a').on('click', function (event) {
+    event.preventDefault();
+    $('#feedback-message').text("That's fine!");
   });
 });
