cf9065cc40c12885d03913a88649e832d6f840bc
Jim Hessin <jhessin@gmail.com>
2021-05-16 14:15:21 -0600

    General Updates
    - Completed Lesson 21

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,47 @@
 import $ from 'jquery';
 
 $(document).ready(() => {
-  $('#input-name').on('keyup', function () {
-    const name = $(this).val();
-    if (name) $('#feedback-message').text(`Pleased to meet you, ${name}`);
-    else $('#feedback-message').text('');
+  $('#button-create-item').on('click', function () {
+    const name = $('#input-create-item').val();
+    $('#input-create-item').val('');
+
+    let html = `
+      <div class="item">
+        <div class="name">${name}</div>
+        <img src="beach.jpg" alt="Beach">
+        <div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium aspernatur error et sunt exercitationem harum dignissimos corporis magni, assumenda earum tenetur voluptas beatae quidem excepturi pariatur impedit rerum! Accusamus, illo?</div>
+        <div class="price">$499</div>
+        <button class='item-add'>Add to cart</button>
+        <button class='item-remove'>Remove</button><br>
+        <a href="#" class='more-info-link'>More info</a>
+        <div class="more-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+      </div>
+    `;
+
+    $('#container').prepend(html);
   });
 
-  $('a').on('click', function (event) {
+  $('#container').on('click', '.item-remove', function () {
+    $(this).parent().remove();
+  });
+
+  $('#container').on('click', '.more-info-link', function (event) {
     event.preventDefault();
-    $('#feedback-message').text("That's fine!");
+    $(this).parent().find('.more-info').slideToggle('fast');
+    $(this)
+      .animate(
+        {
+          opacity: 0.5,
+          'margin-left': 10,
+        },
+        'fast',
+      )
+      .animate(
+        {
+          opacity: 1,
+          'margin-left': 0,
+        },
+        'fast',
+      );
   });
 });
