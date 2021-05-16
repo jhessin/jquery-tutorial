5061434650b2c7652914c1f5c5185d191f5ffd1c
Jim Hessin <jhessin@gmail.com>
2021-05-15 20:52:44 -0600

    Finished Lesson 14

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,11 @@
 import $ from 'jquery';
 
 $(document).ready(() => {
-  $('#select-menu').on('change', function () {
-    const distance = $('#select-menu option:selected').val();
-    const name = $('#select-menu option:selected').text();
-    const price = $('#select-menu option:selected').data('price');
-    if (distance)
-      $('#feedback-message').text(
-        `You are signing up for a ${name}, which costs $${price}, for a distance of ${distance}km`,
-      );
-    else $('#feedback-message').text('');
+  $('#input-name').on('keyup', function () {
+    const name = $(this).val();
+    if (name)
+    $('#feedback-message').text(`Pleased to meet you, ${name}`);
+    else
+    $('#feedback-message').text('');
   });
 });
